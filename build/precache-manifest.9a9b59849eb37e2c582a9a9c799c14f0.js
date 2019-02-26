6eb88ebdfd3bc96ebc5c9abcadde6799bdb4dca6
Bogdan Pashynskyi <bogdanpashynskyi@Bogdans-MacBook-Pro.local>
2019-02-26 18:23:49 +0200

    initial commit

---
+++ build/precache-manifest.9a9b59849eb37e2c582a9a9c799c14f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3353e08bfba1a374c36a",
+    "url": "/todo/static/css/main.163dba4f.chunk.css"
+  },
+  {
+    "revision": "3353e08bfba1a374c36a",
+    "url": "/todo/static/js/main.3353e08b.chunk.js"
+  },
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/todo/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "11a6a103a3c4757ef1e3f6f0b903bb9f",
+    "url": "/todo/index.html"
+  }
+];
